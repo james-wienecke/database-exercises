caa89bb31a9e955c50eb11db698568afdb77f10d
James Wienecke <james.a.wienecke@gmail.com>
2022-01-25 11:40:08 -0600

    Added more quotes to quotes table.

--- quotes_seeder.sql
+++ quotes_seeder.sql
@@ -1,10 +1,11 @@
 USE codeup_test_db;
 
-# INSERT INTO quotes (author_first_name, author_last_name, content)
-# VALUES  ('Douglas', 'Adams', 'Don''t panic.'),
-#         ('Keanu', 'Reeves', 'Whoa...'),
-#         ('Steve', 'Maraboli', 'Happiness is not the absence of problems.');
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES  ('Douglas', 'Adams', 'Don''t panic.'),
+        ('Keanu', 'Reeves', 'Whoa...'),
+        ('Steve', 'Maraboli', 'Happiness is not the absence of problems.'),
+        ('Earnest', 'Hemmingway', 'The world breaks everyone...');
 
-# SELECT id, author_first_name, author_last_name, content FROM quotes;
+SELECT id, author_first_name, author_last_name, content FROM quotes;
 
-TRUNCATE quotes;
+# TRUNCATE quotes;
