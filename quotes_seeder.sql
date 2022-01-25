f2f08ffef4dddf06626ac22b4160e56d3218d7d9
James Wienecke <james.a.wienecke@gmail.com>
2022-01-25 12:05:35 -0600

    Experimented more with other table commands.

--- quotes_seeder.sql
+++ quotes_seeder.sql
@@ -4,7 +4,8 @@ INSERT INTO quotes (author_first_name, author_last_name, content)
 VALUES  ('Douglas', 'Adams', 'Don''t panic.'),
         ('Keanu', 'Reeves', 'Whoa...'),
         ('Steve', 'Maraboli', 'Happiness is not the absence of problems.'),
-        ('Earnest', 'Hemmingway', 'The world breaks everyone...');
+        ('Earnest', 'Hemmingway', 'The world breaks everyone...'),
+        ('Mark', 'Twain', 'Clothes make the man. Naked people have little or no influence on society.');
 
 SELECT id, author_first_name, author_last_name, content FROM quotes;
 
