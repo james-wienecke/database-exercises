4c96edf3cc80286bcb8b989e8f1797e0d8c4ccf8
James Wienecke <james.a.wienecke@gmail.com>
2022-01-25 12:14:24 -0600

    Began work on exercise, added all albums confirmed to have sold >30m copies.

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 49.2, 'Pop, post-disco, funk, rock');
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2, 'Country rock, soft rock, folk rock');
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('Whitney Houston', 'The Bodyguard', 1992, 32.4, 'R&B, soul, pop, soundtrack');
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock');
