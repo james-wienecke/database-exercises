587a312ca37a34439b328f232951095d1402f604
James Wienecke <james.a.wienecke@gmail.com>
2022-01-25 11:06:54 -0600

    Inserted data into quotes table.

--- quotes_seeder.sql
+++ quotes_seeder.sql
@@ -1,3 +1,6 @@
 USE codeup_test_db;
 
-INSERT INTO quotes ();
+INSERT INTO quotes (id, author_first_name, author_last_name, content)
+VALUES (1, 'Douglas', 'Adams', 'Don''t panic');
+
+SELECT author_first_name, content FROM quotes;
