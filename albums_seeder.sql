dc43529120b7338d0d311b1c5665b02e1194f39b
James Wienecke <james.a.wienecke@gmail.com>
2022-01-25 12:18:29 -0600

    Refactored albums_seeder to use a single insert.

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,10 +1,7 @@
 USE codeup_test_db;
 
 INSERT INTO albums(artist, name, release_date, sales, genre)
-VALUES ('Michael Jackson', 'Thriller', 1982, 49.2, 'Pop, post-disco, funk, rock');
-INSERT INTO albums(artist, name, release_date, sales, genre)
-VALUES ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2, 'Country rock, soft rock, folk rock');
-INSERT INTO albums(artist, name, release_date, sales, genre)
-VALUES ('Whitney Houston', 'The Bodyguard', 1992, 32.4, 'R&B, soul, pop, soundtrack');
-INSERT INTO albums(artist, name, release_date, sales, genre)
-VALUES ('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock');
+VALUES  ('Michael Jackson', 'Thriller', 1982, 49.2, 'Pop, post-disco, funk, rock'),
+        ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2, 'Country rock, soft rock, folk rock'),
+        ('Whitney Houston', 'The Bodyguard', 1992, 32.4, 'R&B, soul, pop, soundtrack'),
+        ('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock');
