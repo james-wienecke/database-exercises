0ab26967181de29a75deba9e3839a8b3f16dec08
James Wienecke <james.a.wienecke@gmail.com>
2022-01-26 12:02:42 -0600

    Completed fourth step of part 2 and updated previous step's info output to reflect the distinction between these two queries.

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,9 +4,13 @@ SELECT 'First names of Irena, Vidya, or Maya who are also male' AS 'INFO';
 SELECT * FROM employees
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
 
-SELECT 'Last names starting or ending with "E"' AS 'INFO';
+SELECT 'Last names starting OR ending with "E"' AS 'INFO';
 SELECT * FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT 'Last names starting AND ending with "E"' AS 'INFO';
+SELECT * FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 SELECT 'Last names containing a "Q"' AS 'INFO';
 SELECT * FROM employees
