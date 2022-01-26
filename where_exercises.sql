26fc00d38e14f1df6b206eb25514e73a9714061a
James Wienecke <james.a.wienecke@gmail.com>
2022-01-26 12:01:14 -0600

    Completed third step of part 2.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,12 +1,12 @@
 USE employees;
 
-SELECT 'First names of Irena, Vidya, or Maya' AS 'INFO';
+SELECT 'First names of Irena, Vidya, or Maya who are also male' AS 'INFO';
 SELECT * FROM employees
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
 
-SELECT 'Last names starting with "E"' AS 'INFO';
+SELECT 'Last names starting or ending with "E"' AS 'INFO';
 SELECT * FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
 SELECT 'Last names containing a "Q"' AS 'INFO';
 SELECT * FROM employees
