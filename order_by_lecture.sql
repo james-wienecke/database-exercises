f39fbe6dce6cbaff40644ce77b372582d5a03644
James Wienecke <james.a.wienecke@gmail.com>
2022-01-27 09:36:28 -0600

    Completed order by lecture.

---
+++ order_by_lecture.sql
@@ -0,0 +1,17 @@
+# SELECT column FROM table ORDER BY column_name [ASC|DESC];
+
+SELECT first_name, last_name
+FROM employees.employees
+ORDER BY last_name;
+
+SELECT first_name, last_name
+FROM employees.employees
+ORDER BY last_name DESC;
+
+SELECT first_name, last_name
+FROM employees.employees
+ORDER BY last_name DESC, first_name ASC;
+
+SELECT first_name, last_name, emp_no
+FROM employees.employees
+ORDER BY last_name DESC, first_name ASC, emp_no DESC;
