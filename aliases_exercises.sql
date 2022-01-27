2e42157a812ba6f751b3afefc3e5643aabda0602
James Wienecke <james.a.wienecke@gmail.com>
2022-01-27 16:25:32 -0600

    Finished aliases exercises.

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -3,3 +3,8 @@ USE employees;
 SELECT CONCAT(first_name, ' ', last_name) AS 'full_name'
 FROM employees
 LIMIT 10;
+
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS 'full_name', birth_date AS 'DOB'
+FROM employees
+LIMIT 10;
+
