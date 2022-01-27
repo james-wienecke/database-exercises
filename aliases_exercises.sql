4c135c2b8884ba0b746351c5725d6c82c731716c
James Wienecke <james.a.wienecke@gmail.com>
2022-01-27 16:55:07 -0600

    Cleanup and reworking of aliases.

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,10 +1,10 @@
 USE employees;
 
-SELECT CONCAT(first_name, ' ', last_name) AS 'full_name'
+SELECT CONCAT(emp_no, ' - ', first_name, ' ', last_name) AS 'full_name'
 FROM employees
 LIMIT 10;
 
-SELECT emp_no, CONCAT(first_name, ' ', last_name) AS 'full_name', birth_date AS 'DOB'
+SELECT CONCAT(emp_no, ' - ', first_name, ' ', last_name) AS 'full_name', birth_date AS 'DOB'
 FROM employees
 LIMIT 10;
 
