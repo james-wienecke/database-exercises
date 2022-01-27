1460620f09d8b084147d0adbc2da049f296c6166
James Wienecke <james.a.wienecke@gmail.com>
2022-01-27 09:45:08 -0600

    Completed exercise.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,7 +1,7 @@
 # USE employees;
 
 SELECT * FROM employees.employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY  last_name ASC, first_name ASC;
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY  last_name DESC, first_name DESC;
 
 SELECT * FROM employees.employees
 WHERE last_name LIKE 'E%';
@@ -19,7 +19,8 @@ WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND 
 
 # SELECT 'Last names starting OR ending with "E"' AS 'INFO';
 SELECT * FROM employees.employees
-WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
 
 # SELECT 'Last names starting AND ending with "E"' AS 'INFO';
 SELECT * FROM employees.employees
