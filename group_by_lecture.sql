2c4443ccb6907bd24a0dd75fe1ba5b1ae53d5ee6
James Wienecke <james.a.wienecke@gmail.com>
2022-01-27 15:02:40 -0600

    Finished lecture and began work on exercises for group by.

--- group_by_lecture.sql
+++ group_by_lecture.sql
@@ -5,3 +5,10 @@ FROM employees
 GROUP BY first_name
 ORDER BY COUNT(first_name) DESC
 LIMIT 10;
+
+SELECT hire_date, COUNT(*)
+FROM employees
+GROUP BY hire_date
+ORDER BY hire_date;
+
+SELECT SUM(salary) FROM salaries WHERE YEAR(to_date) = 9999;
